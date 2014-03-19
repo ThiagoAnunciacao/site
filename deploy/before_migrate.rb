734e1feda995c6af69530fee31c09511efccea45
Thiago <thiago.anunciacao@me.com>
2014-03-19 17:17:06 -0300

    change deploy before restart to migrate

--- deploy/before_migrate.rb
+++
@@ -1,7 +0,0 @@
-Chef::Log.info("Running deploy/before_migrate.rb")
-
-rails_env = new_resource.environment["RAILS_ENV"]
-
-Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
-
-run "cd #{release_path} && RAILS_ENV=production bundle exec rake assets:precompile"
