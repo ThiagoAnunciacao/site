33f3c16431645597f23cf1f47a4d159448957fae
Thiago <thiago.anunciacao@me.com>
2014-03-19 16:33:07 -0300

    layout bootstrap3

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,9 +1,11 @@
 Chef::Log.info("Running deploy/before_restart.rb")
 
-# map the environment_variables node to ENV
-# node[:deploy].each do |application, deploy|
-#   deploy[:environment_variables].each do |key, value|
-#     Chef::Log.info("Setting ENV[#{key}] to #{value}")
-#     ENV[key] = value
-#   end
-# end
+rails_env = new_resource.environment["RAILS_ENV"]
+
+Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
+
+execute "rake assets:precompile" do
+  cwd release_path
+  command "bundle exec rake assets:precompile"
+  environment "RAILS_ENV" => rails_env
+end
